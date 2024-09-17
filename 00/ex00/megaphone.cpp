594e1a70485d20d671d230186e5142bc3f783761
outsiderice <arantxamagnelli@gmail.com>
2024-09-17 15:26:59 +0200

    megaphone to test

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,21 +6,34 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 11:38:46 by amagnell          #+#    #+#             */
-/*   Updated: 2024/09/17 12:45:26 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/09/17 15:26:34 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+std::string	str_toupper(std::string &str)
+{
+	std::string::iterator it;
+
+	for (it = str.begin(); it != str.end(); it++)
+		*it = toupper(*it);
+	return (str);
+}
+
 int	main(int argc, char **argv)
 {
-	int	i;
+	std::string	str;
 
-	i = 1;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
+		for(int i = 1; i < argc; i++)
+		{
+			str = static_cast<std::string>(argv[i]);
+			std::cout << str_toupper(str);	
+		}
 	}
 	return (0);
 }
