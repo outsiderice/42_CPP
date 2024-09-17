6a2997fe4c042ea31c01643bc3c0379386d67ad0
outsiderice <arantxamagnelli@gmail.com>
2024-09-17 12:45:59 +0200

    draft

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 11:38:46 by amagnell          #+#    #+#             */
+/*   Updated: 2024/09/17 12:45:26 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+	}
+	return (0);
+}
