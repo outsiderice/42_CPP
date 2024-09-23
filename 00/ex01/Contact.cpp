ad793f83472b7558a6fbea0a5dd15feaf65943d4
outsiderice <arantxamagnelli@gmail.com>
2024-09-23 18:43:44 +0200

    Contact constructor and destructor

---
+++ 00/ex01/Contact.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 15:14:11 by amagnell          #+#    #+#             */
+/*   Updated: 2024/09/23 18:43:23 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+Contact::Contact(void)
+{
+	std::cout << "Contact constructor called" << std::endl;
+	return ;
+}
+
+Contact::~Contact(void)
+{
+	std::cout << "Contact destructor called" << std::endl;
+	return ;
+}
