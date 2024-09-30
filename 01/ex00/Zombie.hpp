197229ffe113bd243fd27b98bfd4198b92f1647d
outsiderice <arantxamagnelli@gmail.com>
2024-09-30 19:22:38 +0200

    starting 01

---
+++ 01/ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 17:43:24 by amagnell          #+#    #+#             */
+/*   Updated: 2024/09/30 17:53:44 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+class	Zombie
+{
+	private :
+
+	public :
+		Zombie(void);
+		~Zombie(void);
+
+		void	announce(void);
+};
+
+#endif
