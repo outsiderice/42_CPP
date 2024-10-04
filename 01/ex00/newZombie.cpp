3ee7706fb92d02e61f50f210a9695c7af17e7fab
outsiderice <arantxamagnelli@gmail.com>
2024-10-04 19:02:41 +0200

    newZombie

---
+++ 01/ex00/newZombie.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 18:33:46 by amagnell          #+#    #+#             */
+/*   Updated: 2024/10/04 19:02:15 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie*	newZombie(std::string name)
+{
+	Zombie	*new_zombie;
+
+	new_zombie = new Zombie(name);
+	return (new_zombie);
+}
