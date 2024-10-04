f47f8c486ac54fa4f84088381687b75514656c26
outsiderice <arantxamagnelli@gmail.com>
2024-10-04 19:05:47 +0200

    randomChump

---
+++ 01/ex00/randomChump.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 19:02:54 by amagnell          #+#    #+#             */
+/*   Updated: 2024/10/04 19:05:30 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void	randomChump(std::string name)
+{
+	Zombie	name;
+
+	name.announce();
+	return ;
+}
