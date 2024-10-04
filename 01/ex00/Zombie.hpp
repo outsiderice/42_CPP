2bf29c6ebd17ace172dd61b67523a0980e30d9b5
outsiderice <arantxamagnelli@gmail.com>
2024-10-04 19:00:41 +0200

    adding fts and changing constructor

--- 01/ex00/Zombie.hpp
+++ 01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:43:24 by amagnell          #+#    #+#             */
-/*   Updated: 2024/10/04 18:30:49 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/10/04 19:00:22 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ class	Zombie
 		std::string	_name;
 
 	public :
-		Zombie(void);
+		Zombie(std::string name);
 		~Zombie(void);
 
 		void	announce(void);
 };
+
+Zombie*	newZombie(std::string name);
+void	randomChump(std::string name);
 
 #endif
