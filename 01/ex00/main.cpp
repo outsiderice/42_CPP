69e21699663cc7fa5aac6da8d554e073da594573
outsiderice <arantxamagnelli@gmail.com>
2024-10-04 19:24:55 +0200

    main

---
+++ 01/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 19:06:20 by amagnell          #+#    #+#             */
+/*   Updated: 2024/10/04 19:24:43 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+int	main(void)
+{
+	std::string	name1;
+	std::string	name2;
+	Zombie		one;
+
+	name1 = "I can be used outside newZombie";
+	name2 = "I live and die in randomChump";
+	one = newZombie(name1);
+	randomChump(name2);
+	delete(one);
+	return (0);
+}
