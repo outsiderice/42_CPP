03c6c27abcfc1ab7c497c6f7cd4b8b4dc107e770
outsiderice <arantxamagnelli@gmail.com>
2024-10-05 13:48:19 +0200

    lunch

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/04 19:43:56 by amagnell          #+#    #+#             */
-/*   Updated: 2024/10/05 13:43:02 by amagnell         ###   ########.fr       */
+/*   Created: 2024/10/05 13:13:35 by amagnell          #+#    #+#             */
+/*   Updated: 2024/10/05 13:42:59 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie*	zombieHorde(int N, std::string name)
+int	main(void)
 {
 	Zombie	*horde;
-	
-	horde = new Zombie[N];
-	for (int i = 0; i < N; i++)
-		horde[i] = Zombie(name);
-	return (horde);
+	int		N = 3;
+
+	horde = zombieHorde(N, "Bob");
+	for(int	i = 0; i < N; i++)
+		horde[i].announce();
+	return (0);
 }
