af55a9d293b67ea10ed07135f6e24a99b7d9a206
outsiderice <arantxamagnelli@gmail.com>
2024-10-08 12:28:31 +0200

    changing to using a class

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,17 +6,27 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 18:50:41 by amagnell          #+#    #+#             */
-/*   Updated: 2024/10/08 09:52:00 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:32:53 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+void	cpy_to_dst(std::ifstream src, char *s1, char *s2)
+{
+	std::ofstream	dst ();
+	std::string		line;
+	
+	while (!src.eof())
+	{
+		getline(src, line);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	std::ifstream	src;
-	std::ofstream	dst;
 
 	if (argc != 4)
 	{
@@ -27,11 +37,11 @@ int	main(int argc, char **argv)
 	src.open(argv[1]);
 	if (src.is_open())
 	{
-		
+		cpy_to_dst(src, argv[2], argv[3]);
 	}
 	else
 	{
-		std::cout << "Problem with src file" << std::endl;
+		std::cout << "Problem with src file." << std::endl;
 		return (1);
 	}
 	src.close();
