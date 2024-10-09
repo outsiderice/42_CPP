e9da8eaeab94e317ed5e4bd63e360dda1629d6a0
outsiderice <arantxamagnelli@gmail.com>
2024-10-09 18:05:13 +0200

    drafting more

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,21 +6,32 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 18:50:41 by amagnell          #+#    #+#             */
-/*   Updated: 2024/10/09 14:10:06 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/10/09 18:04:29 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-void	cpy_to_dst(std::ifstream src, char *s1, char *s2)
+void	cpy_to_dst(std::ifstream src, std::string src_name, std::string *s1, std::string *s2)
 {
-	std::ofstream	dst ();
+	std::fstream	dst(src_name.append(".replace"));
 	std::string		line;
+	size_t			found;
 	
+	if(!dst.is_open())
+	{
+		std::cerr << "File error" << std::endl;
+		std::exit (1);
+	}
 	while (!src.eof())
 	{
 		getline(src, line);
+		found = line.find(s1)
+		if(found != string::npos)
+		{
+			//replace
+		}
 	}
 }
 
@@ -28,33 +39,37 @@ void	check_arguments(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "Wrong number of arguments. Correct usages is:\n";
-		std::cout << "./winner <filename> <s1> <s2>" << std::endl;
-		std::exit(2);
+		std::cerr << "Wrong number of arguments. Correct usages is:\n";
+		std::cerr << "./winner <filename> <s1> <s2>" << std::endl;
+		std::exit(1);
 	}
 	if (!argv[1] || !argv[2] || !argv[3])
 	{
-		std::cout << "Empty arguments. Please enter valid arguments." << std::endl;
-		std::exit(2);
+		std::cerr << "Empty arguments. Please enter valid arguments." << std::endl;
+		std::exit(1);
 	}
 	return ;
 }
 
 int	main(int argc, char **argv)
 {
-	check_arguments(argc, argv);
-	std::ifstream	src(argv[1]);
-	std::string		src_name = argv[1];
-	std::string		s1 = argv[2];
-	std::string		s2 = argv[3];
+	std::ifstream	src;
+	std::string		src_name;
+	std::string		s1;
+	std::string		s2;
 
-	if (src.is_open())
+	check_arguments(argc, argv);
+	src.open(argv[1]);
+	src_name = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+	if (src.is_open() )
 	{
-		cpy_to_dst(src, argv[2], argv[3]);
+		cpy_to_dst(src, src_name, s1, s2);
 	}
 	else
 	{
-		std::cerr << "Problem with src file." << std::endl;
+		std::cerr << "Problem with source file." << std::endl;
 		return (1);
 	}
 	src.close();
