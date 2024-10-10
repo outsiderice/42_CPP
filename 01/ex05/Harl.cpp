2754edfb465da762b5fccdcf16b116528ac7293f
outsiderice <arantxamagnelli@gmail.com>
2024-10-10 16:07:14 +0200

    messages written

--- 01/ex05/Harl.cpp
+++ 01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 15:24:11 by amagnell          #+#    #+#             */
-/*   Updated: 2024/10/10 15:25:54 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:06:58 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,30 @@
 
 void	Harl::debug(void)
 {
+	std::cerr << "Contextual information.\n Mostly used for problem diagnosis." 
+		<< std::endl;
 	return ;
 }
 
 void	Harl::info(void)
 {
-
+	std::cerr << "Contains extensive information. \n"
+		<< "They are helpful for tracing program execution in a production environment" 
+		<< std::endl;
 	return ;
 }
 
 void	Harl::warning(void)
 {
+	std::cerr << "Warning messages indicate a potential issue in the system.\n"
+		<< "Can be handled or ignored." << std::endl;
 	return ;
 }
 void	Harl::error(void)
 {
+	std::cerr << "An unrecoverable error has occurred.\n" 
+		<< "Usually a critical issue which requires manual intervention"
+		<< std::endl;
 	return ;
 }
 void	Harl::complain(std::string level)
