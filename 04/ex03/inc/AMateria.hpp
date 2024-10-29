7aa96b86e10e7b99afb6b251c6a54c0e0a5b7aed
outsiderice <arantxamagnelli@gmail.com>
2024-10-29 19:08:06 +0100

    aah i know

--- 04/ex03/inc/AMateria.hpp
+++ 04/ex03/inc/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:33:02 by amagnell          #+#    #+#             */
-/*   Updated: 2024/10/29 18:54:20 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/10/29 19:07:39 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ class	AMateria
 		
 		std::string const &getType() const;
 
-		virtual AMateria* clone() const = 0;
+		virtual AMateria* clone() const = 0; //lol this is why copying type makes no sense duh
 		virtual void use(ICharacter &target);
 };
 
