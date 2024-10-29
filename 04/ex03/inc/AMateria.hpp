3da3702172f1c2aa9f972b7ea51db154adee00e9
outsiderice <arantxamagnelli@gmail.com>
2024-10-29 17:41:54 +0100

    start ex03

---
+++ 04/ex03/inc/AMateria.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagnell <amagnell@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 17:33:02 by amagnell          #+#    #+#             */
+/*   Updated: 2024/10/29 17:40:43 by amagnell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+#include <iostream>
+
+class	AMateria
+{
+	protected:
+	public:
+		AMateria(std::string const &type);
+		AMateria(const) AMateria &obj;
+		AMateria &operator=(const AMateria &obj);
+		virtual ~AMateria(void);
+		
+		std::string const &getType() const;
+
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter &target);
+};
+
+#endif
