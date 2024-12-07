704fd4e88b0dc96309f8b3a5e4a581bee2271258
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-12-07 16:48:13 +0000

    bureaucrat header

---
+++ 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+    private:
+        std::string     _name;
+        unsigned int    _grade;
+
+    public:
+        Bureaucrat(void);
+        Bureaucrat(const Bureaucrat &obj);
+        Bureaucrat &operator=(const Bureaucrat &obj);
+        ~Bureaucrat(void);
+
+        void    GradeTooHighException() const;
+        void    GradeTooLowException() const;
+
+        std::string     getName(void) const;
+        unsigned int    getGrade(void) const;
+};
+
+#endif
