66c755a6c27b094db5ad1214727c38188d8d7f0f
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-02-04 11:29:03 +0000

    headers folder

--- 05/ex00/inc/Bureaucrat.hpp
+++ 05/ex00/inc/Bureaucrat.hpp
@@ -7,8 +7,8 @@
 class Bureaucrat
 {
     private:
-        std::string     _name;
-        unsigned int    _grade;
+        const std::string	_name;
+        unsigned int		_grade;
 
     public:
         Bureaucrat(void);
