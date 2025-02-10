8f8bcad04e8808765dd0116b2f74e9d2bd90e80b
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-02-10 11:50:36 +0000

    added: Form.hpp

---
+++ 05/ex01/inc/Form.hpp
@@ -0,0 +1,41 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include <iostream>
+#include <exception>
+
+class Form
+{
+    private:
+        std::string	_name;
+        bool		_signed;
+        const int	_sign_grade;
+		const int	_exec_grade;
+
+    public:
+        Form(void);
+        Form(std::string name, int sign_grade, int exec_grade);
+        Form(const Form &obj);
+        Form &operator=(const Form &obj);
+        ~Form(void);
+
+        class    GradeTooHighException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+        class    GradeTooLowException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+
+        std::string     getName(void) const;
+		bool			getSigned(void) const;
+        int    			getSignGrade(void) const;
+		int    			getExecGrade(void) const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Form &Form);
+
+#endif
