348e3a754d8660b28adadd136bf268c8970b5bad
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-02-20 08:31:42 +0000

    Intern added

--- 05/ex03/Intern.cpp
+++ 05/ex03/Intern.cpp
@@ -1,1 +1,25 @@
 #include "Intern.hpp"
+
+Intern::Intern(void)
+{
+	std::cout << "Intern: default constructor called" << std::endl;
+	return ;
+}
+
+Intern::Intern(const Intern &obj)
+{
+	std::cout << "Intern: copy constructor called" << std::endl;
+	return ;
+}
+
+Intern	&Intern::operator=(const Intern &obj)
+{
+	std::cout << "Intern: copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+Intern::~Intern(void)
+{
+	std::cout << "Intern: destructor called" << std::endl;
+	return ;
+}
--- 05/ex03/inc/Intern.hpp
+++ 05/ex03/inc/Intern.hpp
@@ -1,4 +1,15 @@
 #ifndef INTERN_HPP
- #define INTERN_HPP
+#define INTERN_HPP
+
+#include <iostream>
+
+class Intern
+{
+	public:
+		Intern(void);
+		Intern(const Intern &obj);
+		Intern &operator=(const Intern &obj);
+		~Intern(void);
+};
 
 #endif
