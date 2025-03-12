fd0fc64e609ce657ff6e81fc66e358114ce61b08
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-03-12 09:49:04 +0000

    header

--- 06/inc/ScalarConverter.hpp
+++ 06/inc/ScalarConverter.hpp
@@ -6,6 +6,10 @@ class ScalarConverter
     public:
         static void convert(const char *input);
     private:
+        ScalarConverter(void);
+        ScalarConverter(const ScalarConverter &obj);
+        ScalarConverter &operator=(const ScalarConverter &obj);
+        ~ScalarConverter(void);
 };
 
 #endif
