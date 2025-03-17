0110c174a40a6e314c65d7b7e86576e19d5b35bb
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-03-17 11:38:56 +0000

    ocf

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -1,4 +1,24 @@
 #include "ScalarConverter.hpp"
 
 void    ScalarConverter::convert(const char *input)
-{}
+{
+}
+
+//Constructors, operators and destructors
+ScalarConverter::ScalarConverter(void)
+{
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &obj) 
+{
+    (void)obj;
+}
+
+ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &obj)
+{
+    (void)obj;
+	return (*this);
+}
+ScalarConverter::~ScalarConverter(void)
+{
+}
