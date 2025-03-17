e7d423bab95e7ca443c0b7dde7557ced02f4d453
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-03-17 11:08:16 +0000

    main

---
+++ 06/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "ScalarConverter.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Wrong usage: input one argument!\n";
+        std::cout << "  Hint: ./convert argument" << std::endl;
+    }
+    else
+        ScalarConverter::convert(argv[1]);
+    return (0);
+}
