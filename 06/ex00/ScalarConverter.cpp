af8906842446ab0a962ce5e85b0b9f555e49187d
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-03-22 14:23:21 +0000

    identify char

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -1,20 +1,14 @@
 #include "ScalarConverter.hpp"
 
-int  ScalarConverter::sort_type(std::string str)
-{
-    //look for identifying characters in input
-    //return different number for each case
-    // char length == 1 + ischar?
-        // int parse whole string so there's not:
-        // . == double or float
-        // ending f == float
-    //if (str.length() == 1 && str.cbegin()) continue here
-}
-
 void    ScalarConverter::convert(const char *input)
 {
     std::string str = static_cast<std::string>(input);
 
+    if (str.empty())
+    {
+        std::cerr << "Error: input is empty" << std::endl;
+        return ;
+    }
     switch (sort_type(str))
     {
     case 1:
@@ -34,6 +28,27 @@ void    ScalarConverter::convert(const char *input)
     }
     return ;
 }
+
+int  ScalarConverter::sort_type(std::string str)
+{
+    if (str.length() == 1 && isalpha(str.front()) != 0)
+        return (1);
+        // int parse whole string so there's not:
+        // . == double or float
+        // ending f == float
+}
+
+void    ScalarConverter::convert_char(std::string str)
+{}
+
+void    ScalarConverter::convert_int(std::string str)
+{}
+
+void    ScalarConverter::convert_float(std::string str)
+{}
+
+void    ScalarConverter::convert_double(std::string str)
+{}
 
 //Constructors, operators and destructors
 ScalarConverter::ScalarConverter(void)
