b7f9de6ef31f8afeaf59ec544807f5da79df0e69
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-03-27 12:02:07 +0000

    moved int max int min check

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -52,17 +52,16 @@ int  ScalarConverter::sort_type(std::string str)
 
     std::string::size_type  ch = str.find_first_not_of(digits);
     if (str[ch] == std::string::npos)
-    {
-        //check smaller than int max
         return (2);
-    }
     std::string::iterator   it = str.end();
     if (str.at(*it) == 'f')
         f = true;
 }
 
 void    ScalarConverter::convert_char(std::string str)
-{}
+{
+    //also convert to long to check for INT MAX and MIN
+}
 
 void    ScalarConverter::convert_int(std::string str)
 {}
