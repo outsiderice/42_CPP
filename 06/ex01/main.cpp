7d81344b755c4c0ac769b1a99605916d93bfdafb
outsiderice <arantxamagnelli@gmail.com>
2025-04-22 18:22:32 +0200

    added clarification

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -5,10 +5,13 @@
 int	main(void)
 {
 	Data	a;
+
+	std::cout << "Data member number is : " << a.number << ",Data member name is: " << a.name << std::endl;
 	std::cout << "address of a is : " << &a << std::endl;
 	uintptr_t b = Serializer::serialize(&a);
 	std::cout << "converted pointer is : " << b << std::endl;
 	Serializer::deserialize(b);
 	std::cout << "ptr to Data is : " << &a << std::endl;
+	std::cout << "Data member number is : " << a.number << ",Data member name is: " << a.name << std::endl;
 	return (0);
 }
