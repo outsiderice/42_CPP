26ad298d81a92dcdabbdcf0666084e8fba329fa0
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-04-23 13:34:01 +0000

    iirc i need to forward declare

--- 06/ex02/inc/A.hpp
+++ 06/ex02/inc/A.hpp
@@ -1,7 +1,7 @@
 #ifndef A_HPP
 # define A_HPP
 
-#include "Base.hpp"
+class Base;
 
 class	A : public Base
 {
--- 06/ex02/inc/B.hpp
+++ 06/ex02/inc/B.hpp
@@ -1,7 +1,7 @@
 #ifndef B_HPP
 # define B_HPP
 
-#include "Base.hpp"
+class Base;
 
 class	B : public Base
 {
