278ad91fc30dfd50b8b990a02ef555e1735ebef0
outsiderice <arantxamagnelli@gmail.com>
2025-06-05 19:56:13 +0200

    writing array

--- 07/ex02/inc/Array.hpp
+++ 07/ex02/inc/Array.hpp
@@ -4,17 +4,62 @@
 
 template <typename T> class	Array
 {
-	T*;
+	public:
+		Array(void);
+		Array(unsigned int n);
+		Array(const Array &obj);
+		~Array(void);
 
-	Array(void);
-	Array(unsigned int n) : T();
-	Array(Array);
-	
-	int	size() const
-	{
-		int i = 0;
-		while (T[i])
-			i++;
-		return(i);
-	}
+		unsigned int	size() const;
+		class	OutOfBoundsException : public std::exception
+		{
+			public:
+				const char	*what() const throw();
+		}
+
+	private:
+		T	*_arr;
 };
+
+//implementation
+
+Array::Array(void) : _arr(NULL)
+{
+	std::cout << "default constructor called" << std::endl;
+	return ;
+}
+
+Array::Array(unsigned int n)
+{
+	_arr = new T[n];
+	std::cout << "param constructor called" << std::endl;
+	return ;
+}
+
+Array::Array(const Array &obj)
+{
+	if (this != &obj)
+	{
+		
+	}
+	return ;
+}
+
+~Array::Array(void)
+{
+	std::cout << "destructor called" << std::endl;
+	delete[] _arr;
+}
+
+const char	*Array::OutOfBoundsException::what() const throw()
+{
+	return ("Out of bounds");
+}
+
+unsigned int	Array::size()
+{
+	int i = 0;
+	while (T[i])
+		i++;
+	return(i);
+}
