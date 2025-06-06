fc8ab9552c6aae7cf47e7056480f1b1eb9008b90
outsiderice <arantxamagnelli@gmail.com>
2025-06-06 19:21:47 +0200

    basic main

--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include "Array.hpp"
 
-int main( void ) 
+int main( void )
 {
-	<>
+	Array<int>	intArray;
+	Array<char>	charArray(7);
+	try
+	{
+		std::cout << intArray._arr[2] << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception caught" << e.what() << std::endl;
+	}
+	for (int i = 0; i < 7; i++)
+	{
+		charArray._arr[i] = i + 42;
+		std::cout << charArray._arr[i] << std::endl;
+	}
 	return (0);
 }
