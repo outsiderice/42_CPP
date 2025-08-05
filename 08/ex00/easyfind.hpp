3bb67b073a03c2decb2349b100a500bc63e4ac58
outsiderice <arantxamagnelli@gmail.com>
2025-08-05 18:50:58 +0200

    idk

---
+++ 08/ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <algorithm>
+
+template <typename T> easyfind(T where, int what)
+{
+	T<int>::iterator it = where.begin();
+	while () //!= than where.end
+	//if *it == what
+	return () //return *it
+}
+
+#endif
