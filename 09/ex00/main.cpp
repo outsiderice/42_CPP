fdfdae89d42b7cdc0b44d3e11c606b1c1a654d06
A.M.S <99796232+outsiderice@users.noreply.github.com>
2025-08-20 15:10:16 +0000

    store both in class?

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -1,12 +1,23 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	if (argc != 2)
+	{
+		std::cerr << "Needs a file to open." << std::endl;
+		return (1);
+	}
+
+	std::ifstream db;
+	std::string line;
+	db.open();
+	if (!db.is_open())
 	{
 		std::cerr << "Error could not open file." << std::endl;
 		return (1);
 	}
+	db.close();
 	return (0);
 }
